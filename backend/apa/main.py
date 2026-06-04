e1fdbc93a17b3a8f5515d189dc98e27d0af9dc93
Pavana-Harshitha <pavanaharshitha29@gmail.com>
2026-06-04 22:07:44 +0530

    unloaded request

--- backend/apa/main.py
+++ backend/apa/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 import yaml
 from fastapi import FastAPI
+import base64
 
 load_dotenv()
 LANDING_AI_API_KEY=os.getenv("LANDING_AI_API_KEY")
@@ -29,3 +30,20 @@ def apa(request:ProcessRequest):
     extractor_model=config["extractor_model"]
     print(f"parser_model - {parser_model}")
     print(f"extractor_model - {extractor_model}")
+
+    process_name = request.process_name
+    process_code = request.process_code
+    name = request.name
+    content = request.content
+    id = request.id
+    print(f"process_name - {process_name}")
+    print(f"process_code - {process_code}")
+    print(f"name - {name}")
+    print(f"id - {id}")
+
+    modified_file_name = f"{id}_{name}"
+    print(f"modified_file_name - {modified_file_name}")
+    pdf_bytes = base64.b64decode(content)
+    with open(modified_file_name, "wb") as pdf_file:
+        pdf_file.write(pdf_bytes)
+
