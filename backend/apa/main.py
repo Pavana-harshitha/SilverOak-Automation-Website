6532459f45d19440d612b3d9d311476df4266ae5
Pavana-Harshitha <pavanaharshitha29@gmail.com>
2026-06-25 19:22:34 +0530

    Added CORSMiddleware

--- backend/apa/main.py
+++ backend/apa/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from apa_process import router as apa_router
 from table import router as table_router
 
-app=FastAPI()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://silveroak-forms-automation.onrender.com",
+        "http://localhost:5173",
+        "http://127.0.0.1:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(apa_router)
 app.include_router(table_router)
